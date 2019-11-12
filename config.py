482e0b9fdd7694c798f85506a9328ca9ab925242
ysf12374 <47970186+ysf12374@users.noreply.github.com>
2019-11-12 14:56:06 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,49 @@
+import os
+from os.path import join, dirname
+FRENNS_NAME= "vkingsol_frennsdemo"
+CLEARSIGHT_NAME="clearsight_development"
+HOST = "144.76.137.232"
+USER= "vkingsol_demo"
+PSWD= "gUj3z5?9"
+FRENNS_NAME1="vkingsol_frennsblog"
+FRENNS_NAME2="vkingsol_frennsdevelopment"
+FRENNS_NAME3="vkingsol_staging"
+FRENNS_NAME4="vkingsol_testdb"
+FRENNS_NAME5="vkingsol_frennsdevelopment_aug"
+
+
+#Third parties settings.
+CLIENT_SECRET = 'ybR9_pjwYjYZsCdKN4lV6qXkPvxejCkq712lenhG'
+LIVE_STATUS_ENDPOINT = 'https://api.companieshouse.gov.uk/company'
+
+host2 = "144.76.137.232"
+user2 = "vkingsol_demo"
+password2 = "gUj3z5?9"
+db2 = "vkingsol_frennsdevelopment_aug"
+
+
+#Database Settings.
+user3  = 'clearsightapiext'
+password3  = 'gR985xi*'
+db3  = 'clearsight_development'
+host3  = '144.76.137.232'
+# Flask settings.
+FLASK_DEBUG = True  # Do not use debug mode in production
+
+# Server settings.
+SERVER_PORT = 2998
+
+#Third parties settings.
+CLIENT_SECRET = 'ybR9_pjwYjYZsCdKN4lV6qXkPvxejCkq712lenhG'
+LIVE_STATUS_ENDPOINT = 'https://api.companieshouse.gov.uk/company'
+
+#Database Settings.
+MYSQL_DATABASE_USER  = 'vkingsol_demo'
+MYSQL_DATABASE_PASSWORD  = 'gUj3z5?9'
+MYSQL_DATABASE_DB  = 'clearsight_development'
+MYSQL_DATABASE_HOST  = '144.76.137.232'
+
+#Algorithm Settings.
+DATA_FETCH_LIMIT = 10
+LENGTH_FOR_FULL_NAME_SEARCH = 20
+
